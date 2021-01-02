0c03ab0041b4e12a3990f4615926688d284bbebb
kimjinsoo <roorooro9933@gmail.com>
2021-01-02 14:33:35 +0900

    working on the layout

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,23 +1,49 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game Screen!</Text>
+      <Text style={styles.title}>Start a New Game!</Text>
+      {/* 추가 팁은 View는 굉장히 스타일을 적용하는데 Flexible하다는 것이다.
+      그래서 앞으로도 많이 묶어줄 예정이니까 익숙해지도록 하자.
+      */}
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
-    //   모든 space를 get 할 수 있는게 이 방법이라는 것 같다.
-    //  전체를 잡는다? 뭐 그런 의미인듯. 나중에 자세히 알아봐야지 뭐.
-    //  근데 이건 Header 밑에 적용되는거기 때문에 Header를 제외한 모든 영역을 잡는다.
-    //  뭐 이렇게 생각하면 된다는 것 같은데?
     flex: 1,
     padding: 10,
     alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    // 여기서는 flexDirection이 default로 column이기 떄문에
+    // 여기서는 이게 가로 정렬이라는 말이다.
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    // 이걸 하지 않으면 버튼의 길이 만큼만 생기게 되기 때문이다.
+    // Web 이랑 특별히 차이가 나는 곳이니 익숙해지도록 하자.
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
   },
 });
 
