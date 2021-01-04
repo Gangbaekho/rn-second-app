6fc9e1cf917f53bb491e9d64212a1611a53f69f5
kimjinsoo <roorooro9933@gmail.com>
2021-01-04 16:31:54 +0900

    switching between multible screens

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-// 자 이렇게 GameScreen의 props와 관련되 있지 않으면,
-// 또는 GameScreen과 독립적으로 존재해도 될 것 같은 것은
-// 이렇게 밖에다 넣어도 된다는 것이다.
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+
 const generateRandomBetween = (min, max, exclude) => {
-  // 여기는 그냥 비지니스 로직이라서 신경 안써도 된다.
-  // 나중에 내 비지니스 로직이나 잘 짜면 된다.
   min = Math.ceil(min);
   max = Math.floor(max);
   const randomNumber = Math.floor(Math.random() * (max - min)) + min;
@@ -23,12 +21,30 @@ const GameScreen = (props) => {
   );
 
   return (
-    <View>
-      <Text>This is the GameScreen ~~</Text>
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="GREATER" onPress={() => {}} />
+      </Card>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
 
-export default Gamescreen;
+export default GameScreen;
