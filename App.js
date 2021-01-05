c4ea61345c686fab2683f6a092a1b989cc10a232
kimjinsoo <roorooro9933@gmail.com>
2021-01-05 17:54:48 +0900

    adding custom fonts

--- App.js
+++ App.js
@@ -2,16 +2,50 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
+// 뭐 font 사용하려면은 이걸 import 해야 한다는 것 같다.
+import * as Font from "expo-font";
+// 뭐 AppLoading 화면을 보여주는 거겠지. 그리고 Async를 도와주는
+// 역할을 하는 Component라고 생각을 하면 될 듯.
+// 아 이거 하기전에 무조건 expo install expo-app-loading 깔아줘야 함.
+// 안그러면 에러남.
+import AppLoading from "expo-app-loading";
+
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 
+// 이건 App이 Reload 되는것과는 상관 없으니까 밖에다 해서 하는게
+// 바람직하다고 할 수 있음.
+
+const fetchFonts = () => {
+  // Font.loadAsync 라는 것은 Promise를 return 한다.
+  // 뭐 이걸 가져오는데 바로 가져오지는 못하니까
+  // async 적인 것을 이용하기 위해서 Promise를 이용하는거임.
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
-
-  // guessRounds가 0 이상이면 게임 오버를 띄우기 위한 state이다.
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  // 이런식으로 state를 사용해서 하게 하는거구나.
+  // React에서도 비슷하게 사용하면 좋을 것 같긴 하다.
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        // 뭐 밑에있는 것은 말 그대로니까 굳이 설명 안하겠다.
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(error) => console.log(error)}
+      />
+    );
+  }
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
@@ -23,14 +57,12 @@ export default function App() {
     setGuessRounds(0);
   };
 
-  // 뭐 이건 guessRounds의 state를 바꾸기 위한 handler이고
   const gameOverHandler = (numberOfRounds) => {
     setGuessRounds(numberOfRounds);
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  // 상황에 따라서 보이는 Screen이 달라지는 것이라고 로직을 짜는거임.
   if (userNumber && guessRounds <= 0) {
     content = (
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -111,6 +111,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     marginVertical: 10,
+    // 이렇게 원하는 곳에 적용하믄 됨.
+    fontFamily: "open-sans-bold",
   },
   inputContainer: {
     width: 300,
