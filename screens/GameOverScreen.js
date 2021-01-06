bb42064596487c26c4b36da362b7862806596d93
kimjinsoo <roorooro9933@gmail.com>
2021-01-06 14:59:12 +0900

    adding networking iamge

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -8,21 +8,21 @@ const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is over!</TitleText>
-      {/* 특이한 것은 그냥 Image를 첨부하는 것인데도 불구하고
-      View로 감싸고 있다. Styling을 하는데 이것이 더 효율적인 방법이라고
-      소개를 하는 듯 하다. 이런식으로 짜는게 Best practice 인가보다.
-      */}
       <View style={styles.imageContainer}>
         <Image
           style={styles.image}
-          source={require("../assets/success.png")}
-          // 뭐 이게 background-size 를 의미하는 듯 하다
-          // 여기에도 contain 이랑 cover랑 이런게 있으니까
-          // CSS적으로 이해해도 될 것 같다. 아무튼 굳.
-          // cover이 default니까 뭐 굳이 안써도둬 되는거구나.
-          // contain일 떄 정도만 적어주면 될 것 같다.
-          // 그냥 Image를 첨부하는 거지만 약간 Background Image를 하는거랑
-          // 비슷하게 적용되는 것 같다.
+          // 이건 local image를 사용할 떄 쓰는 방법이다.
+          // source={require("../assets/success.png")}
+          // Network를 통해서 가져온 이미지는 항상 Width랑 Height를 꼭 적어줘야 한다는 것을
+          // 조심해야 한다. Local image는 꼭 그래야 할 필요는 없다고 하는 것에 반해서
+          // Network를 통해서 가져온 이미지는 번거롭지만 정해줘야 한다는 것 같네
+          // 아 그리고 또 하나 알아야 할 것은 Network로 가져오는 거기 때문에 아무래도 시간이
+          // 조금 걸리게 된다. 그렇기 떄문에 React native에서는 자동으로 fadeDuration={300}을 적용해 놓는다
+          // 뭐 이걸 조작하고 싶으면은 다른 숫자로 바꾸면 된다.
+          source={{
+            uri:
+              "https://image.shutterstock.com/image-photo/evening-view-ama-dablam-on-260nw-258841592.jpg",
+          }}
           resizeMode="cover"
         />
       </View>
@@ -39,10 +39,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  // imageContainer를 따로 만들어서 거기에다가
-  // 대부분의 Styling을 하고 있다.
-  // 여기서 참고해야 할 것은 overflow인데 저것을 하지 않으면은
-  // 이미지가 튀어나가게 된다. CSS랑 같은 원리로 동작한다고 생각하면 된다.
   imageContainer: {
     borderRadius: 150,
     borderWidth: 3,
@@ -52,8 +48,6 @@ const styles = StyleSheet.create({
     overflow: "hidden",
     marginVertical: 30,
   },
-  // image의 크기는 Container의 100%를 잡게 하는 방식으로
-  // 되어 있다.
   image: {
     width: "100%",
     height: "100%",
