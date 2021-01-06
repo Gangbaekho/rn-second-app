35c1d0ff63b909868c4a4d808f36352fc3de5702
kimjinsoo <roorooro9933@gmail.com>
2021-01-06 14:51:48 +0900

    styling image with image container

--- App.js
+++ App.js
@@ -2,12 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
-// 뭐 font 사용하려면은 이걸 import 해야 한다는 것 같다.
 import * as Font from "expo-font";
-// 뭐 AppLoading 화면을 보여주는 거겠지. 그리고 Async를 도와주는
-// 역할을 하는 Component라고 생각을 하면 될 듯.
-// 아 이거 하기전에 무조건 expo install expo-app-loading 깔아줘야 함.
-// 안그러면 에러남.
 import AppLoading from "expo-app-loading";
 
 import Header from "./components/Header";
@@ -15,13 +10,7 @@ import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 
-// 이건 App이 Reload 되는것과는 상관 없으니까 밖에다 해서 하는게
-// 바람직하다고 할 수 있음.
-
 const fetchFonts = () => {
-  // Font.loadAsync 라는 것은 Promise를 return 한다.
-  // 뭐 이걸 가져오는데 바로 가져오지는 못하니까
-  // async 적인 것을 이용하기 위해서 Promise를 이용하는거임.
   return Font.loadAsync({
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
@@ -33,13 +22,9 @@ export default function App() {
   const [guessRounds, setGuessRounds] = useState(0);
   const [dataLoaded, setDataLoaded] = useState(false);
 
-  // 이런식으로 state를 사용해서 하게 하는거구나.
-  // React에서도 비슷하게 사용하면 좋을 것 같긴 하다.
-
   if (!dataLoaded) {
     return (
       <AppLoading
-        // 뭐 밑에있는 것은 말 그대로니까 굳이 설명 안하겠다.
         startAsync={fetchFonts}
         onFinish={() => setDataLoaded(true)}
         onError={(error) => console.log(error)}
@@ -62,6 +47,14 @@ export default function App() {
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  content = (
+    <GameOverScreen
+      roundsNumber={1}
+      userNumber={1}
+      onRestart={configureNewGameHandler}
+    />
+  );
 
   if (userNumber && guessRounds <= 0) {
     content = (
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is over!</Text>
-      <Text>Number of rounds: {props.roundsNumber}</Text>
-      <Text>Number was : {props.userNumber}</Text>
+      <TitleText>The Game is over!</TitleText>
+      {/* 특이한 것은 그냥 Image를 첨부하는 것인데도 불구하고
+      View로 감싸고 있다. Styling을 하는데 이것이 더 효율적인 방법이라고
+      소개를 하는 듯 하다. 이런식으로 짜는게 Best practice 인가보다.
+      */}
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/success.png")}
+          // 뭐 이게 background-size 를 의미하는 듯 하다
+          // 여기에도 contain 이랑 cover랑 이런게 있으니까
+          // CSS적으로 이해해도 될 것 같다. 아무튼 굳.
+          // cover이 default니까 뭐 굳이 안써도둬 되는거구나.
+          // contain일 떄 정도만 적어주면 될 것 같다.
+          // 그냥 Image를 첨부하는 거지만 약간 Background Image를 하는거랑
+          // 비슷하게 적용되는 것 같다.
+          resizeMode="cover"
+        />
+      </View>
+      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+      <BodyText>Number was : {props.userNumber}</BodyText>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
@@ -17,6 +38,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  // imageContainer를 따로 만들어서 거기에다가
+  // 대부분의 Styling을 하고 있다.
+  // 여기서 참고해야 할 것은 overflow인데 저것을 하지 않으면은
+  // 이미지가 튀어나가게 된다. CSS랑 같은 원리로 동작한다고 생각하면 된다.
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  // image의 크기는 Container의 100%를 잡게 하는 방식으로
+  // 되어 있다.
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 
