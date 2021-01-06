8b8f52d1a8247206a14751db547fe89577e89ea1
kimjinsoo <roorooro9933@gmail.com>
2021-01-06 12:47:32 +0900

    second method to style default stuff

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,9 @@
 import React, { useState, useRef, useEffect } from "react";
-// 다시 한번 상기시키자면, useEffect는 sideEffect와 비슷하다고 생각하면 된다.
-// Rerendering 될떄마다 도는 로직이라고 생각하면 편할 것이다.
 import { View, Text, StyleSheet, Button, Alert } from "react-native";
 
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
+import DefaultStyles from "../constants/default-style";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -25,15 +24,10 @@ const GameScreen = (props) => {
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
 
-  // 이걸로 props.~~ 하는 것을 간단하게 처리하도록 하자.
   const { userChoice, onGameOver } = props;
 
-  // GameScreen이 rendering 될 떄 마다 도는 로직이다.
-  // 기본적으로 그러한데, 특정 state가 변할떄마다 돌게 할 수도 있다는 것이
-  // 특징이 되겠다.
   useEffect(() => {
     if (currentGuess === userChoice) {
-      // GameOverScreen을 띄우도록 조정을 하면 되겠다.
       onGameOver(rounds);
     }
   }, [currentGuess, userChoice, onGameOver]);
@@ -67,7 +61,7 @@ const GameScreen = (props) => {
 
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
+      <Text style={DefaultStyles.bodyText}>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
         <Button title="LOWER" onPress={nextGuessHandler.bind(this, "lower")} />
