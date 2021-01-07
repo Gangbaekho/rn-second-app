f0b3eb265b1b001ffd55b003ebb7892a8044a96f
kimjinsoo <roorooro9933@gmail.com>
2021-01-07 12:29:34 +0900

    adding icons on my projects

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
+
+// icon을 add 하려면은 이러한게 필요하다는 거네.
+// 뭐 Material Icons 도 있고 여러개가 있는것 같은데
+// 당연히 궁금하면 공식 홈페이지 가서 Icons를 찾아봐라
+// 로고가 아니라 아이콘이구나, X 표기라던지, > 라던지 뭐 이런것을
+// 제공하고 있는 곳이라고 생각을 하면 되겠다.
+import { Ionicons } from "@expo/vector-icons";
 
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
@@ -66,10 +73,15 @@ const GameScreen = (props) => {
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
         <MainButton onPress={nextGuessHandler.bind(this, "lower")}>
-          LOWER
+          {/* 여기서 중요한것은 MainButton 들어가보면 알겠찌만 props.children이 Text component
+          안에다가 설정이 되어있다. 여기서 알 수 있는 것은 Icon이 Text component 안에 들어 갈 수 
+          있다는 거지. 
+          name 같은 것은 공식 홈페이지에 가서 찾으면 된다는 것이다.
+          */}
+          <Ionicons name="md-remove" size={24} color="white" />
         </MainButton>
         <MainButton onPress={nextGuessHandler.bind(this, "greater")}>
-          GREATER
+          <Ionicons name="md-add" size={24} color="white" />
         </MainButton>
       </Card>
     </View>
