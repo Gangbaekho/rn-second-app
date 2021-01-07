5234a321235d5f53592f1c7a0982f01172346d43
kimjinsoo <roorooro9933@gmail.com>
2021-01-07 14:52:30 +0900

    using flatlist instead of scrollview

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, Text, StyleSheet, Alert, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import NumberContainer from "../components/NumberContainer";
@@ -20,22 +27,17 @@ const generateRandomBetween = (min, max, exclude) => {
   }
 };
 
-// map에다가 직접 이런걸 설정 할 수 있찌만,
-// 조금 더 분리 하고 싶었다 뭐 그 말인 것 같다
-// 물론 component에서 만들 수 도 있겠찌만
-// 이건 여기서만 쓸꺼니까 여기다가 따로 뺴서 하는 방법을
-// 쓴 것이라고 생각된다.
-const renderListItem = (value, numOfRound) => (
-  <View key={value} style={styles.listItem}>
-    <BodyText>#{numOfRound}</BodyText>
-    <BodyText>{value}</BodyText>
+const renderListItem = (listLength, itemData) => (
+  <View style={styles.listItem}>
+    <BodyText>#{listLength - itemData.index}</BodyText>
+    <BodyText>{itemData.item}</BodyText>
   </View>
 );
 
 const GameScreen = (props) => {
   const initialGuess = generateRandomBetween(1, 100, props.userChoice);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
-  const [pastGuesses, setPastGuesses] = useState([initialGuess]);
+  const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
 
@@ -71,7 +73,10 @@ const GameScreen = (props) => {
     );
 
     setCurrentGuess(nextNumber);
-    setPastGuesses((currentPastGuesses) => [nextNumber, ...currentPastGuesses]);
+    setPastGuesses((currentPastGuesses) => [
+      nextNumber.toString(),
+      ...currentPastGuesses,
+    ]);
   };
 
   return (
@@ -87,14 +92,23 @@ const GameScreen = (props) => {
         </MainButton>
       </Card>
       <View style={styles.listContainer}>
-        {/* 흠 ScrollView를 styling 하려면은 style이 아니라 이 attribute를 
-        이용하라는 거다. 글쎄다 이렇게 해야지 정 가운데로 오긴 하는데,
-        왜 그 상위에 View에다가 alignItems를 center로 하면 원하는 대로 나오지 않았을까.
-        이건 좀 더 개인적으로 테스트를 해야 할 부분인 것 같다.
-        */}
-        <ScrollView contentContainerStyle={styles.list}>
+        {/* <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) => renderListItem(guess, index + 1))}
-        </ScrollView>
+        </ScrollView> */}
+        {/* 
+        예전에 했던 것 같긴 한데, FlatList는 몇개가 나올지 모르는 상황일 때
+        쓰는 Component 였던 것 같다. 복습좀 하긴 해야겠네. 아무튼간에
+        만약에 보여줘야 할 게 1000개이다? 그걸 다 보여주는게 아니라
+        일정 갯수만 보여줘야 겠찌 그래야지 안전할거니까.
+        */}
+        <FlatList
+          keyExtractor={(item) => item}
+          data={pastGuesses}
+          // 신기한 것은 renderListItem에는 두가지 argument가 들어가지만
+          // item 그 자체는 자동으로 주입되기 떄문에 여기에서는 length만 주었다는 거임.
+          renderItem={renderListItem.bind(this, pastGuesses.length)}
+          contentContainerStyle={styles.list}
+        />
       </View>
     </View>
   );
@@ -115,22 +129,11 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     flex: 1,
-    width: "80%",
+    width: "60%",
   },
   list: {
     flexGrow: 1,
-    alignItems: "center",
-    // 흠 이렇게 하면은 ScrollView 에서
-    // Item들이 밑에서 부터 생겨난다. 왜냐면
-    // 수직 정렬을 통해서 flex-end로 했기 떄문에 그렇다.
-    // 반대로 하면 위에서 부터 생기겠찌.
-    // center로 하면 가운데에서부터 생기는건가 음 그렇네
-    // 아이템이 생기는 위치를 이걸로 지정할 수 있겠다.
-    // 뭐야 근데 스크롤이 안되잖아. flex:1을 적용하고 나니까.
-    // 그러니까 flexGrow를 써야한다 뭐 그 말인데.. 잘 모르겠네
-    // 자 다시 생각해 보면 flex:1이란 것은 가능한한 많은 공간을 차지해라 라는것이고
-    // flexGrow도 같은 내용을 포함하고 있지만 ScrollView에서 사용하는 것이라고
-    // 일단은 생각하고 넘어가자, 좀 더 flexible 한 방법이라고 생각하면 된다.
+    // alignItems: "center",
   },
   listItem: {
     borderColor: "#ccc",
@@ -140,9 +143,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     flexDirection: "row",
     justifyContent: "space-around",
-    // 이걸 안하게 되면은 부모의 width:80%의 100%만큼 다 차게 된다 뭐
-    // 이 말인 것 같은데 계속 헷갈리긴 하네.
-    width: "60%",
+    width: "100%",
   },
 });
 
