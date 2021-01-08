a7b4ccfc29fae3396129a2e1c735d928bf46c142
kimjinsoo <roorooro9933@gmail.com>
2021-01-08 17:00:28 +0900

    add scrollview and understanding keyboardavoidingview

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -8,10 +8,9 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
-  // 이건 component로 보기보다는
-  // Object로 보는게 맞다. device의 크기를 알 수 있게 해주는
-  // 그런 Object로 생각해야 한다.
   Dimensions,
+  ScrollView,
+  KeyboardAvoidingView,
 } from "react-native";
 
 import Card from "../components/Card";
@@ -67,44 +66,56 @@ const StartGameScreen = (props) => {
   }
 
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}
-    >
-      <View style={styles.screen}>
-        <TitleText style={styles.title}>Start a New Game!</TitleText>
-        <Card style={styles.inputContainer}>
-          <BodyText>Select a Number</BodyText>
-          <Input
-            style={styles.input}
-            blurOnSubmit
-            autoCapitalize="none"
-            keyboardType="numeric"
-            maxLength={2}
-            onChangeText={numberInputHandler}
-            value={enteredValue}
-          />
-          <View style={styles.buttonContainer}>
-            <View style={styles.button}>
-              <Button
-                title="Reset"
-                onPress={resetInputHandler}
-                color={Colors.accent}
+    // 일단 ScrollView를 넣어줬다.
+    // Landscape Mode로 바꾸니까 세로가 너무 짧아져서
+    // ScrollView를 해놓지 않는 이상 밑에 있는 것을 볼 수가 없었다.
+    <ScrollView>
+      {/* 이걸 했던 이유는 IOS에서 음 뭐랄까. 랜드 스케이프 모드일떄,
+      키보드가 차지하는 화면 비율이 너무 컸다. behavior 같은 경우
+      안드로이드는 padding이 가장 적합하고 IOS는 position이 가장 적당하다고 하는데
+      글쎼다 이게 각각이 무엇을 의미하는지는 정확하게는 모르겠다.
+      */}
+      <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={30}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            Keyboard.dismiss();
+          }}
+        >
+          <View style={styles.screen}>
+            <TitleText style={styles.title}>Start a New Game!</TitleText>
+            <Card style={styles.inputContainer}>
+              <BodyText>Select a Number</BodyText>
+              <Input
+                style={styles.input}
+                blurOnSubmit
+                autoCapitalize="none"
+                keyboardType="numeric"
+                maxLength={2}
+                onChangeText={numberInputHandler}
+                value={enteredValue}
               />
-            </View>
-            <View style={styles.button}>
-              <Button
-                title="Confirm"
-                onPress={confirmInputHandler}
-                color={Colors.primary}
-              />
-            </View>
+              <View style={styles.buttonContainer}>
+                <View style={styles.button}>
+                  <Button
+                    title="Reset"
+                    onPress={resetInputHandler}
+                    color={Colors.accent}
+                  />
+                </View>
+                <View style={styles.button}>
+                  <Button
+                    title="Confirm"
+                    onPress={confirmInputHandler}
+                    color={Colors.primary}
+                  />
+                </View>
+              </View>
+            </Card>
+            {confirmedOutput}
           </View>
-        </Card>
-        {confirmedOutput}
-      </View>
-    </TouchableWithoutFeedback>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
